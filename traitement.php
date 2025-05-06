5e29a148045580cfca3408fa03a9fc1bd10b727c
Casperdev4 <allam.bilal91@gmail.com>
2025-05-06 14:24:23 +0200

    ghjk

--- traitement.php
+++ traitement.php
@@ -63,7 +63,7 @@ try {
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
     $mail->Port       = 465;
 
-    $mail->setFrom('contact@webprime.fr', 'Assainissement 95');
+    $mail->setFrom('contact@webprime.fr', 'IDF 94');
     $mail->addAddress('contact.assainissement@gmail.com');
     $mail->addAddress('webprime91@hotmail.com');
     $mail->CharSet = 'UTF-8';
